744c3110fd7920d242fa71135ef1f9f9152aaafb
Seth Villalon <sethvillalon95@gmail.com>
2022-06-16 13:32:08 -1000

    Finish implementing encryption in all caps

--- CSharp Cipher/Program.cs
+++ CSharp Cipher/Program.cs
@@ -4,12 +4,39 @@ namespace CSharp_Cipher
 {
     class Program
     {
+        public string alphabet { get { return "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; } }
 
         public Program()
         {
             Say("Welcome! Please type in the console the text to encrypt: ");
             string input = Console.ReadLine();
+            int enKey = int.Parse(Console.ReadLine());
+            Say(Encrypt(input.ToUpper(), enKey));
 
+
+        }
+
+        public string Encrypt(string plaintext, int key)
+        {
+            string output = string.Empty;
+            for(int i = 0; i<plaintext.Length; i++)
+            {
+                char letter = plaintext[i];
+                if (!alphabet.Contains(letter))
+                {
+                    output +=letter;
+                }
+                else
+                {
+                    int currentIndex = alphabet.IndexOf(letter);
+                    currentIndex += key;
+                    currentIndex %= 26;
+                    output += alphabet[currentIndex];
+                }
+            }
+            
+            
+            return output;
         }
 
         public static void Say(Object o)
